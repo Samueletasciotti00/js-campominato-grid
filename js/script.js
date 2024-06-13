71a7d917f7f581d45582cd3b5e6fafe7011c9dc9
Samuele <samueletasciotti@icloud.com>
2024-06-13 15:23:06 +0200

    feat: grid container and js const

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//L'utente preme un bottone per generare la griglia di gioco.
+    //Creo il contenitore dei blocchi.
+    const container = document.getElementById('grid');
+
+//Ci sono 10 caselle per ongi riga (10).
+    //Creo dinamicamente le celle
+    
+
+//L'utente cliccando sulla cella, essa si colorera di azzurro
+    //- emette in console con il numero della casella cliccata.
