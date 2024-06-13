271daeea72ea3f0f3a73091cdaf33b5ae3064d3b
Samuele <samueletasciotti@icloud.com>
2024-06-13 21:34:42 +0200

    fix: commits for javascript

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
-//Creo il contenitore dei blocchi.
+//Creare una variabile contenente il riquadro per le caselle;
 const container = document.getElementById('grid');
 
-
-//Ci sono 10 caselle per ongi riga (10).
+//Funzione che generi le caselle all'interno del container grid;
 function generaTabella() {
+    // Ciclo per N caselle;
     for(let i = 0; i < 100; i++){
-        console.log(i); //DEBUG
-
         //Creo dinamicamente le celle
         const box = document.createElement('div');
         //Add Class
@@ -17,8 +15,10 @@ function generaTabella() {
             // Aggiungi una classe aggiuntiva al div cliccato
             box.classList.toggle('bk-blue');
             
+            // Generare un numero casuale per la selezione della casella
             let bomb = Math.floor(Math.random() * 3);
             
+            // Output della casella
             console.log(bomb);
         });
 
@@ -30,6 +30,4 @@ function generaTabella() {
 //L'utente preme un bottone per generare la griglia di gioco.
 document.querySelector('.button-85').addEventListener('click', generaTabella);
 
-
-//Aggiugnere la classe agli elementi appena creati
 
