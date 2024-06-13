d94504a6c9a1a08d8b17c66f05a93d8ceb8ed484
Samuele <samueletasciotti@icloud.com>
2024-06-13 15:53:16 +0200

    feat: layout whit function for js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-//L'utente preme un bottone per generare la griglia di gioco.
+
     //Creo il contenitore dei blocchi.
     const container = document.getElementById('grid');
 
 //Ci sono 10 caselle per ongi riga (10).
-    
+function generaTabella() {
     for(let i = 0; i < 100; i++){
         console.log(i); //DEBUG
 
@@ -15,6 +15,10 @@
         //Inserisco le caselle nel container
         container.append(box);
     }
+}  
+    
+//L'utente preme un bottone per generare la griglia di gioco.
+document.querySelector('.button-85').addEventListener('click', generaTabella);
 
 //L'utente cliccando sulla cella, essa si colorera di azzurro
     //- emette in console con il numero della casella cliccata.
