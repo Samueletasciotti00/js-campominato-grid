77f5fcdf1dbd32e69a3fd647107613b3bd394129
Samuele <samueletasciotti@icloud.com>
2024-06-13 15:36:18 +0200

    feat: layout

--- js/script.js
+++ js/script.js
@@ -3,8 +3,18 @@
     const container = document.getElementById('grid');
 
 //Ci sono 10 caselle per ongi riga (10).
-    //Creo dinamicamente le celle
     
+    for(let i = 0; i < 100; i++){
+        console.log(i); //DEBUG
+
+        //Creo dinamicamente le celle
+        const box = document.createElement('div');
+        //Add Class
+        box.classList.add('square');
+
+        //Inserisco le caselle nel container
+        
+    }
 
 //L'utente cliccando sulla cella, essa si colorera di azzurro
     //- emette in console con il numero della casella cliccata.
